2ed714247c4827b35379c63db30dbbbe231f7057
Radovan Netuka <rnetuka@li-f098124c-3165-11b2-a85c-bf544180b79e.ibm.com>
2026-04-13 15:05:33 +0200

    LLM Chat overhaul

--- apps/ui/admin/src/Pages/LLMChat/mcp.ts
+++ apps/ui/admin/src/Pages/LLMChat/mcp.ts
@@ -1,15 +1,35 @@
 import {Client} from "@modelcontextprotocol/sdk/client/index.js"
 import {SSEClientTransport} from "@modelcontextprotocol/sdk/client/sse.js"
 import {Tool} from "./tools.ts";
-import {getUrl} from "../../custom-fetch.ts"
+//import {getUrl} from "../../custom-fetch.ts"
 
+
+export class McpClient {
+  
+  realMcpClient?: Client
+  
+  async callTool(name: string, args) {
+    if (!this.realMcpClient) {
+      this.realMcpClient = await connectedMCPClient()
+    }
+    return await this.realMcpClient.callTool({
+      name: name,
+      arguments: args
+    })
+  }
+  
+  async close() {
+    return this.realMcpClient?.close()
+  }
+  
+}
 
 export async function connectedMCPClient() {
   const mcpClient = new Client(
     { name: "wanaku-test-client", version: "0.0.2" },
     { capabilities: {} }
   )
-  const url = new URL(getUrl("/public/mcp/sse"))
+  const url = new URL("http://localhost:8020/mcp/sse")//getUrl("/public/mcp/sse"))
   await mcpClient.connect(new SSEClientTransport(url))
   return mcpClient
 }
---
+++ apps/wanaku-router-backend/src/main/java/ai/wanaku/backend/chat/LlmChatResource.java
@@ -0,0 +1,62 @@
+package ai.wanaku.backend.chat;
+
+import jakarta.enterprise.context.ApplicationScoped;
+import jakarta.json.Json;
+import jakarta.json.JsonObject;
+import jakarta.ws.rs.Consumes;
+import jakarta.ws.rs.POST;
+import jakarta.ws.rs.Path;
+import jakarta.ws.rs.Produces;
+import jakarta.ws.rs.WebApplicationException;
+
+import java.io.IOException;
+import java.io.StringReader;
+import java.net.URI;
+import java.net.URISyntaxException;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpRequest.BodyPublishers;
+import java.net.http.HttpResponse;
+import java.net.http.HttpResponse.BodyHandlers;
+
+import static jakarta.ws.rs.core.MediaType.APPLICATION_JSON;
+import static jakarta.ws.rs.core.Response.Status.BAD_REQUEST;
+import static jakarta.ws.rs.core.Response.Status.INTERNAL_SERVER_ERROR;
+import static jakarta.ws.rs.core.Response.Status.TOO_MANY_REQUESTS;
+
+@ApplicationScoped
+@Path("/chat")
+public class LlmChatResource {
+
+    @POST
+    @Path("/completions")
+    @Consumes(APPLICATION_JSON)
+    @Produces(APPLICATION_JSON)
+    public String codeCompletions(String data) {
+        JsonObject json = Json.createReader(new StringReader(data)).readObject();
+        String baseUrl = json.getString("baseUrl");
+        String apiKey = json.getString("apiKey");
+        JsonObject llmParams = json.getJsonObject("chatParams");
+        try (var client = HttpClient.newHttpClient()) {
+            String url = baseUrl + "/v1/chat/completions";
+            HttpRequest request = HttpRequest.newBuilder()
+                    .uri(new URI(url))
+                    .POST(BodyPublishers.ofString(llmParams.toString()))
+                    .header("Content-Type", "application/json")
+                    .header("Authorization", "Bearer " + apiKey)
+                    .build();
+            HttpResponse<String> response = client.send(request, BodyHandlers.ofString());
+            if (response.statusCode() == 429) {
+                throw new WebApplicationException(TOO_MANY_REQUESTS);
+            }
+            if (response.statusCode() != 200) {
+                throw new WebApplicationException(INTERNAL_SERVER_ERROR);
+            }
+            return response.body();
+        } catch (URISyntaxException ex) {
+            throw new WebApplicationException("Malformed base URL", ex, BAD_REQUEST);
+        } catch (IOException | InterruptedException ex) {
+            throw new WebApplicationException(INTERNAL_SERVER_ERROR);
+        }
+    }
+}
